4e0081cf1dfc062018c6586f7717de522235db85
rithu-sha <rithusharajkumar31@gmail.com>
2025-05-09 14:16:24 +0000

    update

--- src/test/java/ProblemSetTest.java
+++ src/test/java/ProblemSetTest.java
@@ -1,7 +1,11 @@
 //don't forget to import anything else you need (ArrayLists, HashMaps, Scanners, etc)
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
+
+import java.beans.Transient;
 import java.io.*;
+
+import javax.smartcardio.Card;
 
 public class ProblemSetTest {
 
@@ -18,4 +22,49 @@ public class ProblemSetTest {
       assertEquals("whiskers", cat.getName());
    }
    */
+
+@Test
+public void testCardGetters()
+{
+   Card card = new Card ("Queen", "Diamonds", 12);
+      assertEquals("Queen", card.getName());
+      assertEquals("Diamonds", card.getSuit());
+      assertEquals(12,card.getValue());
+   }
+
+   @Test
+   public void testCardToString()
+   {
+      Card card = new Card(" Ace", "Hearts", 14);
+       assertEquals("Ace of Hearts", card.toString());
+   }
+
+   @Test
+   public void testCardEquals()
+   {
+       Card card1 = new Card("10","Clubs",10);
+       Card card2 = new Card("10","Clubs",10);
+       Card card3 = new Card("9","Clubs",9);
+   
+   assertTrue(card1.equals(card2));
+
+   assertFalse(card1.equals(card3));
+   }
+
+   @Test 
+   public void
+testDeckInitialization()
+   {
+      Deck deck = new Deck();
+      assertEquals(52,deck.size());
+      assertFalse(deck.isEmpty());
+         }
+   @Test 
+   public void testDeckDrawCard()
+   {
+      Deck deck = new Deck();
+      Card card = deck.drawCard();
+      assertNotNull(card);
+      assertEquals(51,deck.size());
+   }
 }
